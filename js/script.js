067ea557cf130d1a904401a4e9d32e45f384e7b6
Boris Gribovskiy <xinu@yandex.ru>
2015-02-01 01:05:48 +0300

    Removed *.css

--- js/script.js
+++ js/script.js
@@ -1,3 +1,3 @@
 $(document).ready(function(){
-    $("#container").append('<a class="btn-quickview" href="http://www.xland.ru/sw"><span>Quick View</span></a>');
+    $("#container").html('<a class="btn-quickview" href="http://www.xland.ru/sw"><span>Quick View</span></a>');
 });

869f8c4ab722bf669549996788439ed53529bd0b
Boris Gribovskiy <xinu@yandex.ru>
2015-02-01 17:47:03 +0300

    Added less.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
-    $("#container").html('<a class="btn-quickview" href="http://www.xland.ru/sw"><span>Quick View</span></a>');
+    //$(".container").html('<a class="btn-quickview" href="http://www.xland.ru/sw"><span>Quick View</span></a>');
+    var n = 1;
+    $.each($('.container'), function(){
+        if (console.log) {
+            console.log(n);
+        } else {
+            alert(n);
+        }
+        $(this).html('<a class="btn-quickview" href="http://www.xland.ru/sw"><span>Quick View '+n+'</span></a>');
+        n++;
+    });
+    
 });
